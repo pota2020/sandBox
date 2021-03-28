359921878c02f4f05700ab00c81e5f912fbc1d4d
pota2020 <68583553+pota2020@users.noreply.github.com>
2021-03-28 18:34:19 +0900

    Create leetCode102.js
    resolved functional

---
+++ leetCode102.js
@@ -0,0 +1,18 @@
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level). https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+const levelOrder = (root) => {
+    if (!root) return [];
+    return [
+        ...(function* rec(arr) {
+            if (arr.length === 0) return;
+            yield arr.map(x => x.val);
+            yield* rec([...arr.flatMap(x => [x.left, x.right].filter(x => x))]);
+        }([root]))
+    ];
+};
